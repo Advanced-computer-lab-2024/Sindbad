d06ef2baabf1b324e1b5a5c8d6d6cf3063762d2e
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-09-27 15:44:21 +0300

    implement basic route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Profile from "./pages/Profile";
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+
 
 function App() {
     return (
         <main className="bg-dark text-light font-inter">
-            <Profile />
+            <Routes>
+                <Route path="/" element={<MainPage/>} />
+                <Route path="/login" element={<LogIn />} />
+                <Route path="/signup" element={<SignUp />} />
+            </Routes>
         </main>
     );
 }
