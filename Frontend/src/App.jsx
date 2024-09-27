6b367ccf68cf65ad24865d5f30a642872e9d290a
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-09-27 16:44:41 +0300

    add nested routes for app component, and redirection for default path

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,15 +5,21 @@ import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
+import { Navigate } from "react-router-dom/dist";
+import Timeline from "./pages/Timeline";
 
 
 function App() {
     return (
         <main className="bg-dark text-light font-inter">
             <Routes>
-                <Route path="/" element={<MainPage/>} />
+                <Route path="/app" element={<MainPage/>}>
+                    <Route path="profile" element={<Profile />} />
+                    <Route path="timeline" element={<Timeline/>} />
+                </Route>
                 <Route path="/login" element={<LogIn />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/" element={<Navigate to="/app" replace />} />
             </Routes>
         </main>
     );
--- Frontend/src/pages/MainPage.jsx
+++ Frontend/src/pages/MainPage.jsx
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Profile from "./Profile";
+import { Outlet } from "react-router-dom/dist";
 function MainPage() {
+    
     return (
         <div>
             <h1>Main Page</h1>
+            <Outlet />
         </div>
     );
 }
