fad5e16b1c51584a0f74f0d19e6d32c3eb273267
Farah065 <farahahmad065@gmail.com>
2024-10-01 21:45:18 +0300

    set up redux state management for user type and id

--- Frontend/src/utilities/roleConfig.js
+++ Frontend/src/utilities/roleConfig.js
@@ -3,12 +3,12 @@ const rolePermissions = {
     admin: ['Timeline', 'Shopping', 'Profile', 'AccountManagement'],
     guest: ['Timeline'],
     tourist: ['Profile', 'Timeline', 'Shopping'],
-    tour_guide: ['Profile'],
-    tourism_governor: ['Profile'],
+    tourGuide: ['Profile'],
+    tourismGovernor: ['Profile'],
     seller: ['Profile', 'Shopping'],
     advertiser: ['Profile'],
 };
 // Desc: Function to get the pages that a role is allowed to access, based on the rolePermissions map
-function getRolePermissions(role) {
+export function getRolePermissions(role) {
     return rolePermissions[role];
 }
