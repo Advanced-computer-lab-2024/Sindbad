2a6c1467a44f76003f0ebed780dfe8c4b170179c
salmarashad <salmamarashad@gmail.com>
2024-10-04 10:31:26 +0300

    minor fixes in the activity (following conventions)

--- Backend/routes/activityRoutes.js
+++ Backend/routes/activityRoutes.js
@@ -12,17 +12,21 @@ const {
   getSortedActivities,
 } = require("../controllers/activityController");
 
-router
-  .route("/")
-  .post(setActivity)
-  .get(getActivity)
-  .put(updateActivity)
-  .delete(deleteActivity);
-
 router.route("/my-activities").get(getMyActivities);
-router.route("/all-activities").get(getAllActivities);
 router.route("/search").get(searchActivities);
 router.route("/filter").get(filterActivities);
 router.route("/sort").get(getSortedActivities);
+
+router
+  .route("/") // For creating a new activity and getting all activities
+  .post(setActivity)
+  .get(getAllActivities); // Changed to getAllActivities for better clarity
+
+// Updated route to get an activity by ID
+router
+  .route("/:id") // Use a URL parameter for the activity ID
+  .get(getActivity) // Get a specific activity
+  .put(updateActivity) // Update an activity by ID
+  .delete(deleteActivity); // Delete an activity by ID
 
 module.exports = router;
