7fd1023abf7113beaf1a450449353f443f484236
salmarashad <salmamarashad@gmail.com>
2024-10-06 20:16:39 +0300

    changed implementation of filter, search & sort

--- Backend/controllers/activityController.js
+++ Backend/controllers/activityController.js
@@ -41,159 +41,6 @@ const getMyActivities = async (req, res) => {
 };
 
 /**
- * Retrieves all activities from the database.
- *
- * @route GET /activity
- * @param {Object} req - The request object (not used in this function).
- * @param {Object} res - The response object used to send the list of activities or an error message.
- * @returns {Object} - A JSON object containing an array of activities or an error message if an error occurs.
- */
-const getAllActivities = async (req, res) => {
-  try {
-    const activities = await Activity.find();
-    res.status(200).json(activities);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error getting activities",
-      error: error.message,
-    });
-  }
-};
-
-/**
- * Searches for activities by a single search term across name, tags, or category.
- *
- * @route GET /activity/search
- * @param {Object} req - Request with search criteria in query parameters.
- * @param {string} req.query.searchTerm - The term to search for in activity names, tags, or categories.
- * @param {Object} res - Response with matching activities or error.
- * @returns {Object} - A JSON object containing an array of matching activities or an error message.
- */
-const searchActivities = async (req, res) => {
-  try {
-    const { searchTerm } = req.query;
-
-    if (!searchTerm) {
-      return res.status(400).json({ message: "Search term is required" });
-    }
-
-    const regex = new RegExp(searchTerm, "i"); // Case-insensitive search
-
-    // Find activities by name
-    const activitiesByName = await Activity.find({ name: regex });
-
-    // Find activities by category
-    const categories = await Category.find({ name: regex });
-    const categoryIds = categories.map((category) => category._id);
-    const activitiesByCategory = await Activity.find({
-      category: { $in: categoryIds },
-    });
-
-    // Find activities by tags
-    const tags = await Tag.find({ name: regex });
-    const tagIds = tags.map((tag) => tag._id);
-    const activitiesByTags = await Activity.find({ tags: { $in: tagIds } });
-
-    // Combine all found activities
-    const allActivities = [
-      ...activitiesByName,
-      ...activitiesByCategory,
-      ...activitiesByTags,
-    ];
-
-    // Remove duplicates
-    const uniqueActivities = [
-      ...new Set(allActivities.map((activity) => activity._id)),
-    ].map((id) => allActivities.find((activity) => activity._id === id));
-
-    if (uniqueActivities.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "No activities found matching the search term" });
-    }
-
-    res.status(200).json(uniqueActivities);
-  } catch (error) {
-    res.status(500).json({
-      message: "Error searching for activities",
-      error: error.message,
-    });
-  }
-};
-
-/**
- * Filters upcoming activities based on budget, date, category, or rating.
- *
- * @route GET /activity/filter
- * @param {Object} req - Request with filter criteria in query parameters.
- * @param {Object} req.query - Filter criteria.
- * @param {Object} req.query.budget - Object with min and max properties for filtering activities by price.
- * @param {Date} req.query.date.start - Start date for filtering activities.
- * @param {Date} req.query.date.end - End date for filtering activities.
- * @param {string} req.query.category - Category for filtering activities.
- * @param {number} req.query.rating - Minimum rating for filtering activities.
- * @param {Object} res - Response with matching activities or error.
- * @returns {Object} - A JSON object containing an array of matching activities or an error message.
- */
-const filterActivities = async (req, res) => {
-  try {
-    const { budget, date, category, rating } = req.query;
-
-    const filterCriteria = {
-      dateTime: { $gte: new Date() }, // Ensure activities are upcoming
-    };
-
-    if (budget) {
-      filterCriteria.$or = [
-        // For activities where price is a single value
-        { price: { $gte: budget.min, $lte: budget.max } },
-
-        // For activities where price is a range (object with min and max)
-        {
-          "price.min": { $gte: budget.min },
-          "price.max": { $lte: budget.max },
-        },
-      ];
-    }
-
-    // Date filter
-    if (date) {
-      if (date.start) {
-        filterCriteria.dateTime.$gte = new Date(date.start); // Use dateTime for start date
-      }
-      if (date.end) {
-        filterCriteria.dateTime.$lte = new Date(date.end); // Use dateTime for end date
-      }
-    }
-
-    // Category filter
-    if (category) {
-      filterCriteria.category = category;
-    }
-
-    // Rating filter
-    if (rating) {
-      filterCriteria.rating = { $gte: rating };
-    }
-
-    const activities = await Activity.find(filterCriteria);
-
-    if (activities.length === 0) {
-      return res.status(404).json({
-        message: "No upcoming activities found matching the criteria",
-      });
-    }
-
-    res.status(200).json(activities);
-  } catch (error) {
-    res.status(500).json({
-      message: "Error filtering activities",
-      error: error.message,
-    });
-  }
-};
-
-/**
  * Creates a new activity
  *
  * @route POST /activity/
@@ -315,91 +162,156 @@ const deleteActivity = async (req, res) => {
   }
 };
 
-const getSortedActivitiesHelper = async (
-  sortBy,
-  sortOrder = "asc",
-  page = 1,
-  limit = 10
-) => {
+/**
+ * Searches, filters, and sorts activities based on search term, budget, date, category, rating, and sorting options.
+ *
+ * @route GET /activity
+ * @param {Object} req - Request object with search, filter, and sort criteria in query parameters.
+ * @param {string} [req.query.searchTerm] - Optional term to search in activity names, tags, or categories.
+ * @param {Object} [req.query.budget] - Optional for filtering activities by price.
+ * @param {Date} [req.query.date.start] - Optional start date for filtering activities.
+ * @param {Date} [req.query.date.end] - Optional end date for filtering activities.
+ * @param {string} [req.query.category] - Optional category for filtering activities.
+ * @param {number} [req.query.rating] - Optional minimum rating for filtering activities.
+ * @param {string} [req.query.sortBy] - Optional field to sort by ("price" or "rating").
+ * @param {string} [req.query.sortOrder="asc"] - Optional sort order ("asc" or "desc"). Default is "asc".
+ * @param {number} [req.query.page=1] - Optional page number for pagination. Default is 1.
+ * @param {number} [req.query.limit=10] - Optional number of activities per page. Default is 10.
+ * @param {Object} res - Response with matching, sorted, and paginated activities or error.
+ * @returns {Object} - A JSON object containing an array of activities or an error message.
+ */
+const getActivities = async (req, res) => {
   try {
-    let sortCriteria = {};
-    const order = sortOrder === "asc" ? 1 : -1;
-    const skip = (page - 1) * limit;
+    const {
+      searchTerm,
+      budget,
+      date,
+      category,
+      rating,
+      sortBy,
+      sortOrder = "asc",
+      page = 1,
+      limit = 10,
+    } = req.query;
 
-    if (sortBy === "price") {
-      sortCriteria = {
-        $addFields: {
-          priceValue: {
-            $cond: {
-              if: { $isNumber: "$price" },
-              then: "$price",
-              else: "$price.min",
-            },
-          },
-        },
-      };
-    } else if (sortBy === "rating") {
-      sortCriteria = { rating: order };
-    } else {
-      throw new Error("Invalid sortBy parameter. Use 'price' or 'rating'.");
+    // Set default filter for upcoming activities
+    const filterCriteria = {
+      dateTime: { $gte: new Date() },
+    };
+
+    // Apply budget filter if provided
+    if (budget) {
+      const parsedBudget = Number(budget);
+      filterCriteria.$or = [
+        { price: { $lte: parsedBudget } }, // For fixed price activities
+        { "price.max": { $lte: parsedBudget } }, // For activities with price ranges
+      ];
     }
 
-    // Ensure limit is a number
-    const limitNumber = Number(limit);
+    // Apply date filter if provided
+    if (date) {
+      if (date.start) {
+        filterCriteria.dateTime.$gte = new Date(date.start); // Start date
+      }
+      if (date.end) {
+        // Set end date to the end of the day
+        const endDate = new Date(date.end);
+        endDate.setHours(23, 59, 59, 999); // Set time to the end of the day
+        filterCriteria.dateTime.$lte = endDate; // End date
+      }
+    }
 
-    const activities = await Activity.aggregate([
-      ...(sortBy === "price" ? [sortCriteria] : []),
-      ...(sortBy === "price"
-        ? [{ $sort: { priceValue: order } }]
-        : [{ $sort: { [sortBy]: order } }]),
-      { $skip: skip }, // Skip for pagination
-      { $limit: limitNumber }, // Use limitNumber instead of limit
-    ]);
+    // Apply category filter if provided
+    if (category) {
+      filterCriteria.category = category;
+    }
 
-    return activities;
-  } catch (error) {
-    console.error("Error fetching sorted activities:", error);
-    throw error;
-  }
-};
+    // Apply rating filter if provided
+    if (rating) {
+      filterCriteria.rating = { $gte: rating };
+    }
 
-/**
- * Fetches sorted activities with pagination
- *
- * @route GET activity/sort
- * @param {Object} req - Request object
- * @param {string} req.query.sortBy - Field to sort by ("price" or "rating")
- * @param {string} [req.query.sortOrder="asc"] - Sort order ("asc" or "desc"). Default is "asc"
- * @param {number} [req.query.page=1] - Page number for pagination. Default is 1
- * @param {number} [req.query.limit=10] - Number of activities per page. Default is 10
- *
- * @param {Object} res - Response
- *
- * @returns {void} - Sends the sorted activities or an error message
- */
-const getSortedActivities = async (req, res) => {
-  const { sortBy, sortOrder = "asc", page = 1, limit = 10 } = req.query;
-  try {
-    const activities = await getSortedActivitiesHelper(
-      sortBy,
-      sortOrder,
-      page,
-      limit
-    );
+    // Fetch activities that match the filter criteria
+    let activities = await Activity.find(filterCriteria);
+
+    // Apply search term filter if provided
+    if (searchTerm) {
+      const regex = new RegExp(searchTerm, "i"); // Case-insensitive search
+      const activitiesByName = await Activity.find({ name: regex });
+      const categories = await Category.find({ name: regex });
+      const categoryIds = categories.map((category) => category._id);
+      const activitiesByCategory = await Activity.find({
+        category: { $in: categoryIds },
+      });
+      const tags = await Tag.find({ name: regex });
+      const tagIds = tags.map((tag) => tag._id);
+      const activitiesByTags = await Activity.find({
+        tags: { $in: tagIds },
+      });
+
+      const allSearchedActivities = [
+        ...activitiesByName,
+        ...activitiesByCategory,
+        ...activitiesByTags,
+      ];
+
+      const uniqueSearchedActivities = [
+        ...new Set(allSearchedActivities.map((activity) => activity._id)),
+      ].map((id) =>
+        allSearchedActivities.find((activity) => activity._id === id)
+      );
+
+      // Filter original activities list with searched activities
+      activities = activities.filter((activity) =>
+        uniqueSearchedActivities.some(
+          (searchedActivity) =>
+            searchedActivity._id.toString() === activity._id.toString()
+        )
+      );
+    }
+
+    if (activities.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No activities found matching the criteria" });
+    }
+
+    // Apply sorting
+    const order = sortOrder === "asc" ? 1 : -1;
+
+    if (sortBy === "price") {
+      activities = activities.sort((a, b) =>
+        order === 1
+          ? getPriceValue(a) - getPriceValue(b)
+          : getPriceValue(b) - getPriceValue(a)
+      );
+    } else if (sortBy === "rating") {
+      activities = activities.sort((a, b) =>
+        order === 1 ? a.rating - b.rating : b.rating - a.rating
+      );
+    }
+
+    function getPriceValue(product) {
+      return product.price.min || product.price;
+    }
+
+    // Apply pagination
+    const skip = (page - 1) * limit;
+    activities = activities.slice(skip, skip + Number(limit));
+
     res.status(200).json(activities);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      message: "Error searching, filtering, or sorting activities",
+      error: error.message,
+    });
   }
 };
-
 module.exports = {
   setActivity,
   getActivity,
   updateActivity,
   deleteActivity,
   getMyActivities,
-  getAllActivities,
-  searchActivities,
-  filterActivities,
-  getSortedActivities,
+  getActivities,
 };
