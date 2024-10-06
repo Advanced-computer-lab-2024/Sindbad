62a883968940504fbae16a37cef63a6bbc35829e
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2024-10-06 23:53:01 +0300

    Link itinerary view with data

--- Frontend/src/components/custom/Card.jsx
+++ Frontend/src/components/custom/Card.jsx
@@ -1,4 +1,5 @@
 import ImagePlaceholder from "@/components/custom/ImagePlaceholder";
+import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Star, StarHalf, Bookmark, Edit3, CircleX } from 'lucide-react';
 import {
@@ -12,49 +13,77 @@ import GenericForm from "./genericForm";
 import DeleteForm from "./deleteForm";
 
 function Card({ data, id, userId, type }) {
+    const navigate = useNavigate();
     return (
-        <article className="w-full flex flex-col border border-primary-700/80 rounded-md overflow-clip bg-gradient-to-br from-dark to-primary-900/50 group">
-            <div className="h-[156px] relative shrink-0">
-                <ImagePlaceholder />
-                {type === "tourist" &&
-                    <button className="absolute top-2 right-2 border-2 border-dark opacity-0 group-hover:opacity-100 transition-all hover:border-secondary bg-primary-900 p-1.5 rounded-full">
-                        <Bookmark fill="currentColor" size={16} />
-                    </button>
-                }
-                {type !== "tourist" && id === userId &&
-                    <div>
-                        <Dialog>
-                            <DialogTrigger className="absolute top-2 right-2 border-2 border-dark opacity-0 group-hover:opacity-100 transition-all hover:border-secondary bg-primary-900 p-1.5 rounded-full">
-                                <Edit3 fill="currentColor" size={16} />
-                            </DialogTrigger>
-                            <DialogContent className="overflow-y-scroll max-h-[50%]">
-                                <DialogHeader>
-                                    <GenericForm type={type === "seller" ? "product" : type === "advertiser" ? "activity" : type === "tourGuide" ? "itinerary" : "site"} id={id} data={data} />
-                                </DialogHeader>
-                            </DialogContent>
-                        </Dialog>
-                        <Dialog>
-                            <DialogTrigger className="absolute top-12 right-2 border-2 border-dark opacity-0 group-hover:opacity-100 transition-all hover:border-secondary bg-primary-900 p-1.5 rounded-full">
-                                <CircleX size={16} />
-                            </DialogTrigger>
-                            <DialogContent className="overflow-y-scroll max-h-[50%]">
-                                <DialogTitle>
-                                    Are you sure you want to delete this {type === "advertiser" ? "activity" : type === "tourGuide" ? "itinerary" : "site"}?
-                                </DialogTitle>
-                                <DialogHeader>
-                                    <DeleteForm type={type === "advertiser" ? "activity" : type === "tourGuide" ? "itinerary" : "site"} data={data} />
-                                </DialogHeader>
-                            </DialogContent>
-                        </Dialog>
-                    </div>
-                }
-            </div>
-            <div className="flex flex-col p-3 gap-5 h-full justify-between">
-                <div>
-                    <h4 className="text-base font-semibold line-clamp-2">
-                        {data.name}
-                    </h4>
-                    {/* <div className="shrink-0 text-secondary flex gap-0.5 items-center">
+			<article className="w-full flex flex-col border border-primary-700/80 rounded-md overflow-clip bg-gradient-to-br from-dark to-primary-900/50 group">
+				<div className="h-[156px] relative shrink-0">
+					<ImagePlaceholder />
+					{type === "tourist" && (
+						<button className="absolute top-2 right-2 border-2 border-dark opacity-0 group-hover:opacity-100 transition-all hover:border-secondary bg-primary-900 p-1.5 rounded-full">
+							<Bookmark fill="currentColor" size={16} />
+						</button>
+					)}
+					{type !== "tourist" && id === userId && (
+						<div>
+							<Dialog>
+								<DialogTrigger className="absolute top-2 right-2 border-2 border-dark opacity-0 group-hover:opacity-100 transition-all hover:border-secondary bg-primary-900 p-1.5 rounded-full">
+									<Edit3 fill="currentColor" size={16} />
+								</DialogTrigger>
+								<DialogContent className="overflow-y-scroll max-h-[50%]">
+									<DialogHeader>
+										<GenericForm
+											type={
+												type === "seller"
+													? "product"
+													: type === "advertiser"
+													? "activity"
+													: type === "tourGuide"
+													? "itinerary"
+													: "site"
+											}
+											id={id}
+											data={data}
+										/>
+									</DialogHeader>
+								</DialogContent>
+							</Dialog>
+							<Dialog>
+								<DialogTrigger className="absolute top-12 right-2 border-2 border-dark opacity-0 group-hover:opacity-100 transition-all hover:border-secondary bg-primary-900 p-1.5 rounded-full">
+									<CircleX size={16} />
+								</DialogTrigger>
+								<DialogContent className="overflow-y-scroll max-h-[50%]">
+									<DialogTitle>
+										Are you sure you want to delete this{" "}
+										{type === "advertiser"
+											? "activity"
+											: type === "tourGuide"
+											? "itinerary"
+											: "site"}
+										?
+									</DialogTitle>
+									<DialogHeader>
+										<DeleteForm
+											type={
+												type === "advertiser"
+													? "activity"
+													: type === "tourGuide"
+													? "itinerary"
+													: "site"
+											}
+											data={data}
+										/>
+									</DialogHeader>
+								</DialogContent>
+							</Dialog>
+						</div>
+					)}
+				</div>
+				<div className="flex flex-col p-3 gap-5 h-full justify-between">
+					<div>
+						<h4 className="text-base font-semibold line-clamp-2">
+							{data.name}
+						</h4>
+						{/* <div className="shrink-0 text-secondary flex gap-0.5 items-center">
                         <Star fill="#fcd34d" size={16} />
                         <Star fill="#fcd34d" size={16} />
                         <Star fill="#fcd34d" size={16} />
@@ -64,23 +93,19 @@ function Card({ data, id, userId, type }) {
                             (1092)
                         </p>
                     </div> */}
-                    <p className="text-xs leading-[11px] font-medium text-neutral-500 mt-1">
-                        Rating: {data.rating ? `${data.rating} / 5` : "N/A"}
-                    </p>
-                </div>
-                <Button
-                    onClick={() => console.log(data)}
-                >
-                    <p className="text-xs">
-                        Read more
-                    </p>
-                    <div className="shrink-0">
-                        <ArrowRight size={13} />
-                    </div>
-                </Button>
-            </div>
-        </article>
-    );
+						<p className="text-xs leading-[11px] font-medium text-neutral-500 mt-1">
+							Rating: {data.rating ? `${data.rating} / 5` : "N/A"}
+						</p>
+					</div>
+					<Button onClick={() => navigate(`/app/itinerary/${data._id}`)}>
+						<p className="text-xs">Read more</p>
+						<div className="shrink-0">
+							<ArrowRight size={13} />
+						</div>
+					</Button>
+				</div>
+			</article>
+		);
 }
 
 export default Card;
--- Frontend/src/pages/ItineraryView.jsx
+++ Frontend/src/pages/ItineraryView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
 	Accessibility,
 	MapPin,
@@ -10,11 +10,10 @@ import {
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import GoogleMapRead from "@/components/custom/maps/GoogleMapRead";
+import { getItineraryById } from "@/services/ItineraryApiHandler";
+import { useParams } from "react-router-dom";
 
 function getRandomRating() {
-	{
-		/*return (Math.floor(Math.random() * 6));*/
-	}
 	return (Math.round(Math.random() * 10) / 2).toFixed(1);
 }
 
@@ -22,159 +21,111 @@ function getRandomReviews() {
 	return Math.floor(Math.random() * 1000) + 1;
 }
 
-const reviews = getRandomReviews();
-const rating = Math.floor(getRandomRating());
-const fullStars = rating;
-const emptyStar = 5 - fullStars;
+function handleItineraryValues(itinerary) {
+	if (!itinerary.rating) {
+		itinerary.rating = getRandomRating();
+	}
 
-// const accessibilityFeatures = [
-//     { icon: <Accessibility/>, label:"Mobility aid friendly" },
-//     { icon: <EarOff/>, label:"Hearing impaired support" },
-//     { icon: <EyeOff/>, label:"Vision impaired support" },
-//     { icon: <Speech/>, label:"Text-to-speech devices" },
-// ];
+    if (!itinerary.description) {
+        itinerary.description = "Lorem Ipsum...";
+    }
 
-const itinerary = {
-	_id: "66fec47696a5a727ea518498",
-	name: "Updated Weekend Getaway to the Mountains",
-	activities: [
-		"603fba4f7b3b1a23f44c5555", // Example ObjectId for an activity
-		"603fba4f7b3b1a23f44c6666", // Example ObjectId for another activity
-	],
-	locations: [
-		{
-			address: "123 Main St, Cityville",
-			coordinates: {
-				lat: 40.7128,
-				lng: -74.006,
-			},
-		},
-		{
-			address: "456 Park Ave, Cityville",
-			coordinates: {
-				lat: 41.713,
-				lng: -71.007,
-			},
-		},
-		{
-			address: "789 Park Ave, Cityville",
-			coordinates: {
-				lat: 42.713,
-				lng: -72.007,
-			},
-		},
-		{
-			address: "456 Park Ave, Cityville",
-			coordinates: {
-				lat: 49.713,
-				lng: -99.007,
-			},
-		},
-		{
-			address: "789 Park Ave, Cityville",
-			coordinates: {
-				lat: 41.713,
-				lng: -72.007,
-			},
-		},
-	],
-	timeline: [
-		"Day 1: Arrival and Relaxation",
-		"Day 2: Hiking and Sightseeing",
-		"Day 3: Departure with a bonus activity",
-	],
-	duration: 4, // Duration in days
-	languages: ["English", "French"],
-	price: 349.99,
-	availableDatesTimes: [
-		new Date("2024-11-15T10:00:00.000Z"),
-		new Date("2024-11-22T10:00:00.000Z"),
-	],
-	accessibility: ["Wheelchair accessible", "Family-friendly", "Pet-friendly"],
-	pickUpLocation: "City Center",
-	dropOffLocation: "Updated Mountain Resort Parking Lot",
-	creatorId: "66f81ff63ba5a08e8581e95e",
-	headCount: 6,
-	rating: 4.5,
-};
+	if (
+		itinerary.price !== null &&
+		typeof itinerary.price === "object" &&
+		typeof itinerary.price.min === "number" &&
+		typeof itinerary.price.max === "number"
+	) {
+		itinerary.price = (itinerary.price.min + itinerary.price.max) / 2;
+	}
 
-const dates = itinerary.availableDatesTimes.map((date) => {
-	const d = new Date(date);
-	// Format the date parts (weekday, day, month)
-	const weekday = d.toLocaleString("en-US", { weekday: "short" }); // Full weekday name
-	const day = d.toLocaleString("en-US", { day: "numeric" }); // Day of the month
-	const month = d.toLocaleString("en-US", { month: "short" }); // Full month name
+	if (!itinerary.reviews) {
+		itinerary.reviews = getRandomReviews();
+	}
+}
 
-	return `${weekday} ${day} ${month}`; // Returning formatted string
-});
-
-const times = itinerary.availableDatesTimes.map((date) => {
-	const d = new Date(date);
-	return d.toLocaleString("en-US", {
-		hour: "numeric",
-		minute: "numeric",
-		hour12: true,
-	}); // Example: "10:00 AM"
-});
-
-function Itinerary() {
+const Itinerary = () => {
 	// State to store the current count
 	const [adult, setAdult] = useState(0);
 	const [child, setChild] = useState(0);
-
-	const handleAdultIncrement = () => {
-		setAdult(adult + 1);
-	};
-
-	// Function to handle decrement for adult
-	const handleAdultDecrement = () => {
-		if (adult > 0) {
-			setAdult(adult - 1);
-		}
-	};
-
-	// Function to handle increment for child
-	const handleChildIncrement = () => {
-		setChild(child + 1);
-	};
-
-	// Function to handle decrement for child
-	const handleChildDecrement = () => {
-		if (child > 0) {
-			setChild(child - 1);
-		}
-	};
-
-	// To store which element to border
-
+	const { itineraryId } = useParams();
+	const [itinerary, setItinerary] = useState(null); // Initialize as null
 	const [selectedDate, setSelectedDate] = useState(0);
 	const [selectedTime, setSelectedTime] = useState(0);
+
+
+
+	const getItinerary = async () => {
+		let response = await getItineraryById(itineraryId);
+		console.log(response);
+
+		if (response.error) {
+			console.error(response.message);
+		} else {
+            handleItineraryValues(response);
+			setItinerary(response);
+		}
+	};
+
+	useEffect(() => {
+		getItinerary();
+	}, []);
+
+	// Ensure itinerary is not null or undefined before rendering
+	if (!itinerary) {
+		console.log("itinerary: " + itinerary);
+		return <p>Loading...</p>; // You can return a loading spinner or message here
+	}
+
+	// Ensure availableDatesTimes exists before accessing it
+	if (!itinerary.availableDatesTimes) return null;
+
+	const fullStars = itinerary.rating;
+	const emptyStar = 5 - fullStars;
+
+	const dates = itinerary.availableDatesTimes.map((date) => {
+		const d = new Date(date);
+		const weekday = d.toLocaleString("en-US", { weekday: "short" });
+		const day = d.toLocaleString("en-US", { day: "numeric" });
+		const month = d.toLocaleString("en-US", { month: "short" });
+		return `${weekday} ${day} ${month}`;
+	});
+
+	const times = itinerary.availableDatesTimes.map((date) => {
+		const d = new Date(date);
+		return d.toLocaleString("en-US", {
+			hour: "numeric",
+			minute: "numeric",
+			hour12: true,
+		});
+	});
+
+	const handleAdultIncrement = () => setAdult(adult + 1);
+	const handleAdultDecrement = () => adult > 0 && setAdult(adult - 1);
+	const handleChildIncrement = () => setChild(child + 1);
+	const handleChildDecrement = () => child > 0 && setChild(child - 1);
 
 	return (
 		<div className="min-h-screen flex justify-center items-center bg-primary-950">
 			<div className="w-full max-w-7xl px-8 py-8 bg-primary-900 shadow-lg rounded-md">
 				<div className="grid grid-cols-1 lg:grid-cols-3 gap-10 p-8">
 					<div>
-						{/*Title Section*/}
 						<div className="my-4">
-							<h1 className="text-4xl font-bol">{itinerary.name}</h1>
+							<h1 className="text-4xl font-bold">{itinerary.name}</h1>
 							<p className="text-light text-lg">
-								{itinerary.duration} day trip{" "}
+								{itinerary.duration} day trip
 							</p>
 
 							{/*Star Section */}
-							<div className=" relative flex gap-4">
-								<div className=" flex">
+							<div className="relative flex gap-4">
+								<div className="flex">
 									{Array.from({ length: fullStars }, (_, index) => (
 										<Star
 											key={index}
-											className=" fill-secondary"
+											className="fill-secondary"
 											strokeWidth={0}
 										/>
 									))}
-
-									{/*hasHalfStar && <StarHalf fill="yellow" strokeWidth={0} />*/}
-
 									{Array.from({ length: emptyStar }, (_, index) => (
 										<Star
 											key={fullStars + index}
@@ -184,22 +135,19 @@ function Itinerary() {
 									))}
 								</div>
 								<p className="text-light">
-									{rating}/5 ({reviews})
+									{itinerary.rating}/5 ({itinerary.reviews})
 								</p>
 							</div>
 						</div>
 
 						<p className="text-light">
-							This bus tour offers you the chance to explore the city of London
-							at your own pace. You can choose between a 24-hour, 48-hour, or
-							72-hour pass and visit the top landmarks and tourist sites in the
-							city.
+							{itinerary.description}
 						</p>
 
-						{/*Supported Languages*/}
+						{/* Supported Languages */}
 						<div className="my-6">
 							<h2 className="text-xl font-semibold pb-2">
-								Supported Langauges
+								Supported Languages
 							</h2>
 							<div className="flex flex-wrap gap-2">
 								{itinerary.languages.map((lang) => (
@@ -213,15 +161,8 @@ function Itinerary() {
 							</div>
 						</div>
 
-						{/*Accessibility Fts*/}
-						{/*TODO: add icons*/}
-						<div className=" flex flex-wrap gap-4">
-							{/* {accessibilityFeatures.map((feature, index) => (
-                                <p key={index} className="flex items-center gap-2">
-                                    {feature.icon}
-                                    {feature.label}
-                                </p>
-                            ))} */}
+						{/* Accessibility Features */}
+						<div className="flex flex-wrap gap-4">
 							{itinerary.accessibility.map((feature) => (
 								<p key={feature} className="flex items-center gap-2">
 									<Star className="h-4 w-4" />
@@ -231,12 +172,11 @@ function Itinerary() {
 						</div>
 					</div>
 
-					{/*TODO: Fix img placeholders + padding*/}
 					<div className="grid grid-cols-2 col-span-2 gap-1">
-						<div className="bg-light h-full rounded-lg "></div>
+						<div className="bg-light h-full rounded-lg"></div>
 						<div className="">
-							<div className="bg-light h-1/2 rounded-lg mb-px "></div>
-							<div className="bg-light h-1/2 rounded-lg "></div>
+							<div className="bg-light h-1/2 rounded-lg mb-px"></div>
+							<div className="bg-light h-1/2 rounded-lg"></div>
 						</div>
 					</div>
 				</div>
@@ -247,14 +187,14 @@ function Itinerary() {
 				<div className="grid grid-cols-9 grid-rows gap-8 mt-8 p-8">
 					<div className="col-span-3">
 						<h2 className="text-2xl font-semibold mb-4">Itinerary</h2>
-						<ul className="">
+						<ul>
 							<li>
 								<div className="flex items-start space-x-2">
 									<div className="mt-1">
 										<MapPin size={40} className="border-2 rounded-full p-1" />
 									</div>
 									<div>
-										<p>Starting at, </p>
+										<p>Starting at,</p>
 										<p>{itinerary.pickUpLocation}</p>
 										<a href="#" className="text-secondary">
 											See details
@@ -265,13 +205,10 @@ function Itinerary() {
 							<li>
 								<div className="relative border border-light rounded-full w-1 h-1 bg-light left-5 mb-3"></div>
 							</li>
-							<li>
-								<div className="relative border border-light rounded-full w-1 h-1 bg-light left-5 mb-3"></div>
-							</li>
 							{itinerary.timeline.map((stop, index) => (
 								<div key={index}>
 									<li>
-										<div className="flex items-start- space-x-2">
+										<div className="flex items-start space-x-2">
 											<div className="flex-shrink-0 bg-light text-dark font-semibold w-10 h-10 flex items-center justify-center rounded-full">
 												{index + 1}
 											</div>
@@ -286,9 +223,6 @@ function Itinerary() {
 									<li>
 										<div className="relative border border-light rounded-full w-1 h-1 bg-light left-5 mb-3"></div>
 									</li>
-									<li>
-										<div className="relative border border-light rounded-full w-1 h-1 bg-light left-5 mb-3"></div>
-									</li>
 								</div>
 							))}
 							<li>
@@ -297,7 +231,7 @@ function Itinerary() {
 										<MapPin size={40} className="border-2 rounded-full p-1" />
 									</div>
 									<div>
-										<p>Finishing at, </p>
+										<p>Finishing at,</p>
 										<p>{itinerary.dropOffLocation}</p>
 										<a href="#" className="text-secondary">
 											See details
@@ -308,17 +242,15 @@ function Itinerary() {
 						</ul>
 					</div>
 
-					{/* Map Placeholder*/}
 					<div className="col-span-3 bg-light h-full w-full rounded-lg">
-						{/* <GoogleMapRead
-							key={selectedLocation}
-							lat={itinerary.locations[selectedLocation].coordinates.lat}
-							lng={itinerary.locations[selectedLocation].coordinates.lng}
-						/> */}
+						{/* Google Map Read component */}
+						<GoogleMapRead
+							key={selectedDate}
+							lat={itinerary.locations[selectedDate]?.coordinates?.lat}
+							lng={itinerary.locations[selectedDate]?.coordinates?.lng}
+						/>
 					</div>
 
-					{/*Availibility Section
-                    TODO: add conditional for selected element to have thicker border, seperate day and month to make them appear vertical, lookup map docs*/}
 					<div className="col-span-3 p-4">
 						<h2 className="text-2xl font-semibold mb-4">Search Availability</h2>
 						<div className="grid grid-cols-4 gap-2">
@@ -328,7 +260,7 @@ function Itinerary() {
 									onClick={() => setSelectedDate(idx)}
 									className={`border py-2 px-6 min-h-20 max-w-24 rounded-lg bg-primary-700 text-center ${
 										selectedDate === idx
-											? "border-light border-2 border- "
+											? "border-light border-2"
 											: "border-transparent"
 									}`}
 								>
@@ -442,9 +374,9 @@ function Itinerary() {
 							{/* Total Cost Section */}
 							<div className="border-t border-dashed mt-4 pt-4">
 								<p>
-									Total:{" "}
+									Total:
 									<span className="font-semibold text-xl mx-4">
-										{(adult * itinerary.price + child * itinerary.price) * 1.2}
+										{adult * itinerary.price + child * itinerary.price}
 									</span>
 									LE
 								</p>
@@ -463,5 +395,6 @@ function Itinerary() {
 			</div>
 		</div>
 	);
-}
+};
+
 export default Itinerary;
