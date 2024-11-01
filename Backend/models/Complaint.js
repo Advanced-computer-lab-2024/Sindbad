25d9bce02f03b0a13e3002354e2289ab65bf927e
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2024-11-01 11:20:28 +0200

    Implement complaint model

---
+++ Backend/models/Complaint.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const complaintSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: [true, "Please add a complaint title"],
+		},
+		body: {
+			type: String,
+			required: [true, "Please add a complaint body"],
+		},
+		date: {
+			type: Date,
+			default: Date.now,
+		},
+		isResolved: {
+			type: Boolean,
+			default: false,
+		},
+		comment: {
+			type: String,
+			default: "",
+		},
+		creatorId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Tourist",
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("Complaint", complaintSchema);
