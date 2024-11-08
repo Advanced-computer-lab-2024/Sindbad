067b8ff5965a7349eb792a98cc25f393d37c5758
FieryPheonix <jana.elowainy@gmail.com>
2024-11-08 21:19:57 +0200

    Finished 3rd party flight booking

---
+++ Backend/models/Flight.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+// Define the schema for the Tourist model
+const FlightSchema = new mongoose.Schema(
+    {
+        BookingNumber: {
+            type: String,
+			required: true,
+        },
+        FlightNumber: {
+            type: [String],
+			required: true,
+        },
+        Duration: {
+            type: [Number],
+			required: true,
+        },
+        DepartureLocation: {
+            type: [String],
+			required: true,
+        },
+        ArrivalLocation: {
+            type: [String],
+			required: true,
+        },
+        DepartureDateTime: {
+            type: [Date],
+			required: true,
+        },
+        ArrivalDateTime: {
+            type: [Date],
+			required: true,
+        },
+        price: {
+            type: String,
+			required: true,
+        },
+        travelerID: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Tourist",
+            required: true,
+        },
+    },
+    { timestamps: true }
+  );
+  
+  // Create the Tourist model
+  const Flight = mongoose.model("Flight", FlightSchema);
+  
+  module.exports = Flight;
