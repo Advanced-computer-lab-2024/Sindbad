113780df0f8544ca828a71971670200162d17042
Farah065 <farahahmad065@gmail.com>
2024-11-10 13:08:30 +0200

    made hotel booking page the correct size

--- Frontend/src/pages/HotelBooking.jsx
+++ Frontend/src/pages/HotelBooking.jsx
@@ -73,9 +73,9 @@ function HotelBooking() {
 
 
 	return (
-		<div className="py-8 px-24 max-w-[1200px] flex flex-col gap-4 mx-auto">
+		<div className="">
 			<div className="flex items-center gap-6 mb-6">
-				<h1 className="text-3xl font-extrabold">Featured Hotels</h1>
+				<h1 className="text-3xl font-extrabold shrink-0">Featured Hotels</h1>
 				<hr className="border-neutral-300 border w-full mt-1.5" />
 			</div>
 			<div className="flex gap-10">
