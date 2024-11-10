f18c6f66d6fc0b39bb17d8058c6b91fd7cdedc68
Mohamed Emad <mohamedemadfahmy0@gmail.com>
2024-11-10 13:42:03 +0200

    Add prettier.config to root directory of project

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: false, // Enforces the use of double quotes for strings
+  useTabs: false, // Use spaces instead of tabs
+  tabWidth: 2, // Set the number of spaces per indentation level
+};
