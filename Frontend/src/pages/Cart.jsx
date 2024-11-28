0d7954fd91f488c5382e139a400c872a9de6c170
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-11-28 13:21:19 +0200

    fetch cart and display

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -1,9 +1,60 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+import { useEffect, useState } from "react";
+import { getCart } from "@/services/TouristApiHandler";
+import { useUser } from "@/state management/userInfo";
+
 export const Cart = () => {
 
-    
+    const [cart, setCart] = useState([]);
+    const { id } = useUser();
+
+    const fetchCart = async () => {
+        try {
+            const response = await getCart(id);
+            setCart(response);
+        } catch (error) {
+            console.error("Error fetching cart:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchCart();
+    }, [id]);
+
     return (
         <div>
-            <h1>Cart</h1>
+            <Table>
+                {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+                <TableHeader>
+                    <TableRow>
+                    <TableHead className="w-[100px]">Product</TableHead>
+                    <TableHead>Quantity</TableHead>
+                    <TableHead>Price</TableHead>
+                    <TableHead className="text-right">Remove</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {   cart?.map((item, key) => {
+                            return (
+                                <TableRow key={key}>
+                                    <TableCell className="font-medium">{item.productID.name}</TableCell>
+                                    <TableCell>{item.quantity}</TableCell>
+                                    <TableCell>{item.productID.price * item.quantity}</TableCell>
+                                    <TableCell className="text-right">Remove</TableCell>
+                                </TableRow>
+                            );
+                        })
+                    }
+                </TableBody>
+            </Table>
         </div>
     );
 }
