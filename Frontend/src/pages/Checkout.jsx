667b4aaed29a84d63e6c262abfe31dd63b878da6
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-11-29 22:11:16 +0200

    finish frontend

--- Frontend/src/pages/Checkout.jsx
+++ Frontend/src/pages/Checkout.jsx
@@ -23,6 +23,7 @@ import { forms } from '@/components/custom/genericForm/forms';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export const Checkout = () => {
 
@@ -112,99 +113,165 @@ export const Checkout = () => {
             Sindbad Checkout
             </h1>
             <div className="grid grid-cols-3 grid-flow-col gap-4 items-start">
-                <div className="col-span-2 bg-white rounded-md p-6">
-                    {chosenAddress === "null" &&
-                        <div>
-                            {
-                                addresses.length < 1
-                                ? 
-                                (<div>
-                                    <h2 className='text-lg font-bold mb-4'>
-                                        It seems you havent added an address yet. Please add an address to continue.
-                                    </h2>
-                                    <GenericForm type={"touristAddress"} id={id} fetcher={fetchAddress}/>
-                                </div>) 
-                                :
-                                (<div>
-                                    <h2 className='text-lg font-bold mb-4'>
-                                        Choose an Address to Deliver to
-                                    </h2>
-                                    <div className="grid gap-4">
-                                        {addresses.map((item) => (
-                                            <div 
-                                            key={item[0]._id} 
-                                            className="bg-gray-100 p-4 rounded-md text-sm cursor-pointer hover:bg-gray-200 transition-colors"
-                                            onClick={() => setChosenAddress(item[0])}
-                                            >
-                                                <div className='mb-2 font-semibold'>
-                                                    {item[0].label}
-                                                </div>
-                                                <div>
-                                                    {item[0].street}
-                                                </div>
-                                                <div>
-                                                    {item[0].city}
-                                                </div>
-                                                <div>
-                                                    {item[0].state}{","} {item[0].country}
-                                                </div>
-                                                {item[0].zip}
-                                            </div>
-                                        ))}
-                                    </div>
-                                    {
-                                        addNewAddress && 
-                                        <Form {...form}>
-                                            <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+                <div className='col-span-2'>
+                    <div className="bg-white rounded-md p-6">
+                        {chosenAddress === "null" ?
+                            (
+                            <div>
+                                {
+                                    addresses.length < 1
+                                    ? 
+                                    (<div>
+                                        <h2 className='text-lg font-bold mb-4'>
+                                            It seems you havent added an address yet. Please add an address to continue.
+                                        </h2>
+                                        <GenericForm type={"touristAddress"} id={id} fetcher={fetchAddress}/>
+                                    </div>) 
+                                    :
+                                    (<div>
+                                        <h2 className='text-lg font-bold mb-4'>
+                                            Choose an Address to Deliver to
+                                        </h2>
+                                        <div className="grid gap-4">
+                                            {addresses.map((item) => (
                                                 <div 
-                                                    className="bg-gray-100 p-4 rounded-md hover:bg-gray-200 transition-colors mt-4 group flex flex-col gap-2"
+                                                key={item[0]._id} 
+                                                className="bg-gray-100 p-4 rounded-md text-sm cursor-pointer hover:bg-gray-200 transition-colors"
+                                                onClick={() => setChosenAddress(item[0])}
                                                 >
                                                     <div className='mb-2 font-semibold'>
-                                                            <Input
-                                                            {...form.register("label")}
-                                                            className=' focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='Label' />
+                                                        {item[0].label}
                                                     </div>
                                                     <div>
-                                                        <Input
-                                                        {...form.register("street")}
-                                                        className='focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='Street' />
+                                                        {item[0].street}
                                                     </div>
                                                     <div>
-                                                        <Input
-                                                        {...form.register("city")}
-                                                        className='focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='City' />
+                                                        {item[0].city}
                                                     </div>
                                                     <div>
-                                                        <Input 
-                                                        {...form.register("state")}
-                                                        className='focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='State' />
-                                                        {" , "} 
-                                                        <Input 
-                                                        {...form.register("country")}
-                                                        className='focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='Country' />
+                                                        {item[0].state}{","} {item[0].country}
                                                     </div>
-                                                    <Input 
-                                                    {...form.register("zip")}
-                                                    className='focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='Zip' />
-                                                    <Button type="submit" className="bg-dark text-white w-max">
-                                                        Submit
-                                                    </Button>
+                                                    {item[0].zip}
                                                 </div>
-                                                
-                                            </form>
-                                        </Form>
-                                    }
-                                    <a 
-                                    className='block mt-4 text-base hover:text-yellow-400 cursor-pointer w-max'
-                                    onClick={() => {
-                                        setAddNewAddress(true);
-                                    }}
+                                            ))}
+                                        </div>
+                                        {
+                                            addNewAddress && 
+                                            <Form {...form}>
+                                                <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+                                                    <div 
+                                                        className="bg-gray-100 p-4 rounded-md hover:bg-gray-200 transition-colors mt-4 group flex flex-col gap-2"
+                                                    >
+                                                        <div className='mb-2 font-semibold'>
+                                                                <Input
+                                                                {...form.register("label")}
+                                                                className=' focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='Label' />
+                                                        </div>
+                                                        <div>
+                                                            <Input
+                                                            {...form.register("street")}
+                                                            className='focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='Street' />
+                                                        </div>
+                                                        <div>
+                                                            <Input
+                                                            {...form.register("city")}
+                                                            className='focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='City' />
+                                                        </div>
+                                                        <div>
+                                                            <Input 
+                                                            {...form.register("state")}
+                                                            className='focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='State' />
+                                                            {" , "} 
+                                                            <Input 
+                                                            {...form.register("country")}
+                                                            className='focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='Country' />
+                                                        </div>
+                                                        <Input 
+                                                        {...form.register("zip")}
+                                                        className='focus:border-0 bg-transparent transition-colors inline w-max border-0 focus:bg-white' placeholder='Zip' />
+                                                        <Button type="submit" className="bg-dark text-white w-max">
+                                                            Submit
+                                                        </Button>
+                                                    </div>
+                                                    
+                                                </form>
+                                            </Form>
+                                        }
+                                        <a 
+                                        className='block mt-4 text-base hover:text-yellow-400 cursor-pointer w-max'
+                                        onClick={() => {
+                                            setAddNewAddress(true);
+                                        }}
+                                        >
+                                            Add an address
+                                        </a>
+                                    </div>)
+                                }
+                            </div>
+                            )
+                            :
+                            (<div>   
+                                <div>
+                                    <h2 className='text-lg font-bold mb-4'>
+                                        Your Chosen Address is:
+                                    </h2>
+                                    <div  
+                                    className="bg-gray-100 p-4 rounded-md text-sm hover:bg-gray-200 transition-colors"
                                     >
-                                        Add an address
-                                    </a>
-                                </div>)
-                            }
+                                        <div className='mb-2 font-semibold'>
+                                            {chosenAddress.label}
+                                        </div>
+                                        <div>
+                                            {chosenAddress.street}
+                                        </div>
+                                        <div>
+                                            {chosenAddress.city}
+                                        </div>
+                                        <div>
+                                            {chosenAddress.state}{","} {chosenAddress.country}
+                                        </div>
+                                        {chosenAddress.zip}
+                                    </div>
+                                </div>
+                                <div className=' mt-4'>
+
+                                </div>
+                            </div>
+                            )
+                        }
+                    </div>
+                    {chosenAddress !== "null" &&
+                    <div className="bg-white rounded-md p-6 mt-4">
+                        <h2 className='text-lg font-bold mb-4'>Please Choose a Payment Method</h2>
+                    <Tabs defaultValue="account" className="w-[400px] mx-auto">
+                        <TabsList>
+                            <TabsTrigger value="credit">
+                                <img className="h-6"src="https://taxidermyplanet.com/wp-content/uploads/2018/10/transparent-logos-credit-card-1.png" />
+                            </TabsTrigger>
+                            <TabsTrigger value="cod">Cash On Delivery</TabsTrigger>
+                            <TabsTrigger value="wallet">Sindbad Wallet</TabsTrigger>
+                        </TabsList>
+                        <div className='mt-4'>
+                            <TabsContent value="credit">
+                                <h3 className='text-sm text-center'>
+                                    Pay with your Credit Card through our partner vendor, Stripe.
+                                </h3>
+
+                            </TabsContent>
+                            <TabsContent value="cod">
+                                <h3 className='text-sm text-center'>
+                                    Pay the delivery person when they arrive with your order.
+                                </h3>
+                            </TabsContent>
+                            <TabsContent value="wallet">
+                                <h3 className='text-sm text-center'>
+                                    Pay with your Sindbad Wallet.
+                                </h3>
+                            </TabsContent>
                         </div>
+                    </Tabs>
+                        
+                    </div>
                     }
                 </div>
                 <div className="col-span-1 bg-white rounded-md p-6">
