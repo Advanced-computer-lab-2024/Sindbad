52b9e231453dc4736503e7ced055944e185fc6f3
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-11-30 15:18:26 +0200

    cart empty check

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -93,87 +93,96 @@ export const Cart = () => {
 
     return (
         <div className="py-8 px-24 max-w-[1200px] mx-auto bg-gradient-to-b from-neutral-200/60 to-light border border-neutral-300 rounded-md">
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[100px]">Product</TableHead>
-                        <TableHead>Quantity</TableHead>
-                        <TableHead>Price</TableHead>
-                        <TableHead className="text-right">Remove</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {cart?.map((item) => (
-                        <TableRow key={item.productID._id}>
-                            <TableCell className="font-medium">{item.productID.name}</TableCell>
-                            <TableCell className="flex items-center space-x-2">
-                                <button
-                                    className="px-2 py-1 border rounded-md hover:bg-neutral-300"
-                                    onClick={() =>
-                                        handleQuantityChange(
-                                            item.productID._id,
-                                            Math.max(1, item.quantity - 1)
-                                        )
-                                    }
-                                >
-                                    -
-                                </button>
-                                <span>{item.quantity}</span>
-                                <button
-                                    className="px-2 py-1 border rounded-md hover:bg-neutral-300"
-                                    onClick={() =>
-                                        handleQuantityChange(
-                                            item.productID._id,
-                                            item.quantity + 1
-                                        )
-                                    }
-                                >
-                                    +
-                                </button>
-                            </TableCell>
-                            <TableCell>
-                                {convertedPrices[item.productID._id] !== undefined
-                                    ? convertedPrices[item.productID._id].toFixed(2)
-                                    : "Loading..."}
-                            </TableCell>
-                            <TableCell className="text-right pr-5">
-                                <BadgeX
-                                    className="ml-auto hover:fill-red-500 hover:text-white text-red-500 cursor-pointer"
-                                    onClick={async () => {
-                                        try {
-                                            await removeItemFromCart(
-                                                id,
-                                                item.productID._id
-                                            );
-                                            fetchCart();
-                                        } catch (error) {
-                                            console.error(
-                                                "Error removing item from cart:",
-                                                error
-                                            );
-                                        }
-                                    }}
-                                />
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+            {cart.length === 0 ? (
+
+                <div>
+                    <h2 className="text-center">Your cart is empty</h2>
+                </div>
+            )
+            : 
             <div>
-                <h2 className="text-right mt-4">
-                    Total:{" "}
-                    {Object.values(convertedPrices).length
-                        ? Object.values(convertedPrices).reduce((total, price) => total + price, 0).toFixed(2)
-                        : "Loading..."}
-                    {" " + currency}          
-                </h2>
-                <Button
-                    className="mt-4 ml-auto max-w-[200px] w-full text-center justify-center"
-                    onClick={() => navigate("/checkout")}
-                >
-                    Checkout
-                </Button>
-            </div>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[100px]">Product</TableHead>
+                            <TableHead>Quantity</TableHead>
+                            <TableHead>Price</TableHead>
+                            <TableHead className="text-right">Remove</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {cart?.map((item) => (
+                            <TableRow key={item.productID._id}>
+                                <TableCell className="font-medium">{item.productID.name}</TableCell>
+                                <TableCell className="flex items-center space-x-2">
+                                    <button
+                                        className="px-2 py-1 border rounded-md hover:bg-neutral-300"
+                                        onClick={() =>
+                                            handleQuantityChange(
+                                                item.productID._id,
+                                                Math.max(1, item.quantity - 1)
+                                            )
+                                        }
+                                    >
+                                        -
+                                    </button>
+                                    <span>{item.quantity}</span>
+                                    <button
+                                        className="px-2 py-1 border rounded-md hover:bg-neutral-300"
+                                        onClick={() =>
+                                            handleQuantityChange(
+                                                item.productID._id,
+                                                item.quantity + 1
+                                            )
+                                        }
+                                    >
+                                        +
+                                    </button>
+                                </TableCell>
+                                <TableCell>
+                                    {convertedPrices[item.productID._id] !== undefined
+                                        ? convertedPrices[item.productID._id].toFixed(2)
+                                        : "Loading..."}
+                                </TableCell>
+                                <TableCell className="text-right pr-5">
+                                    <BadgeX
+                                        className="ml-auto hover:fill-red-500 hover:text-white text-red-500 cursor-pointer"
+                                        onClick={async () => {
+                                            try {
+                                                await removeItemFromCart(
+                                                    id,
+                                                    item.productID._id
+                                                );
+                                                fetchCart();
+                                            } catch (error) {
+                                                console.error(
+                                                    "Error removing item from cart:",
+                                                    error
+                                                );
+                                            }
+                                        }}
+                                    />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <div>
+                    <h2 className="text-right mt-4">
+                        Total:{" "}
+                        {Object.values(convertedPrices).length
+                            ? Object.values(convertedPrices).reduce((total, price) => total + price, 0).toFixed(2)
+                            : "Loading..."}
+                        {" " + currency}          
+                    </h2>
+                    <Button
+                        className="mt-4 ml-auto max-w-[200px] w-full text-center justify-center"
+                        onClick={() => navigate("/checkout")}
+                    >
+                        Checkout
+                    </Button>
+                </div>
+            </div>}
         </div>
     );
 };
